1bf0e6ee484dfe7215af1444ab3dc84b89b8972e
kieran <hello@kieranbamforth.me>
2023-02-20 14:38:39 +0000

    Parameterise GRPC hostname

--- web/app.py
+++ web/app.py
@@ -2,15 +2,16 @@ import grpc
 import cv2
 import numpy as np
 import tensorflow as tf
-import uuid
+import os
 
 from flask import Flask, render_template, request
 from tensorflow_serving.apis import prediction_service_pb2_grpc, predict_pb2
 
+GRPC_HOST = os.getenv("GRPC_HOST", default="localhost")
 GRPC_PORT = "8500"
 GRPC_MAX_RECEIVE_MESSAGE_LENGTH = 4096 * 4096 * 3  # Max LENGTH the GRPC should handle
 
-channel = grpc.insecure_channel(f'localhost:{GRPC_PORT}',
+channel = grpc.insecure_channel(f'{GRPC_HOST}:{GRPC_PORT}',
     options=[('grpc.max_receive_message_length', GRPC_MAX_RECEIVE_MESSAGE_LENGTH)])
 
 prediction_service = prediction_service_pb2_grpc.PredictionServiceStub(channel)
@@ -31,11 +32,10 @@ def index():
         )
 
 def predict(request):
-    filename = f'/home/vscode/src/predict-handwritten-numbers/web/uploads/${uuid.uuid4()}.png'
     file = request.files['file']
-    file.save(filename)
 
-    imageData = cv2.imread(filename, cv2.IMREAD_GRAYSCALE).astype('float32')
+    npImage = np.fromstring(file.read(), np.uint8)
+    imageData = cv2.imdecode(npImage, cv2.IMREAD_GRAYSCALE).astype('float32')
 
     resizedImageData = cv2.resize(imageData, (28, 28))
 
